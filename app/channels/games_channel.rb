7e0d89b16a499b00d5a659c68c079fb270d6409d
Minh Nguyen <minh.n@hey.com>
2025-06-25 17:19:46 -0400

    PKM-23 refactor

--- app/channels/games_channel.rb
+++ app/channels/games_channel.rb
@@ -1,7 +1,9 @@
 class GamesChannel < ApplicationCable::Channel
   before_subscribe :init_game
+  # after_subscribe :set_game
   after_unsubscribe :cleanup_game
 
+  # CHANNEL CALLBACKS
   def connect
     puts "Connecting to GamesChannel"
     # Any setup needed when channel is connected
@@ -9,6 +11,10 @@ class GamesChannel < ApplicationCable::Channel
 
   def subscribed
     stream_for @game
+  end
+
+  def receive(data)
+    puts "Received data: #{data.inspect}"
   end
 
   def cleanup_game
@@ -27,181 +33,50 @@ class GamesChannel < ApplicationCable::Channel
     get_game
   end
 
-  def get_game(reset = false)
-    game = Game.find(@game.id)
-    GamesChannel.broadcast_to(@game,
-      {
-        game: {
-          id: game.id,
-          state: game.state,
-          players: game.players.map do |player|
-            {
-              id: player.guest_id,
-              # can_flip: can_flip(player),
-              score: game.game_cards.select { |gc| gc.scored_by == player.guest_id }.length
-            }
-          end,
-          cards: game.game_cards.sort_by(&:position).map { |gc| generate_card(gc) },
-          winner: game.winner_id.nil? ? nil : game.winner_id
-        },
-        can_flip: {
-          "#{game.players[0]&.guest_id}": can_flip(game.players[0]),
-          "#{game.players[1]&.guest_id}": can_flip(game.players[1])
-        },
-        reset:
-      }
-    )
-    # ActionCable.server.broadcast("GamesChannel:#{@game.id}", { game: @game.attributes })
-  end
-
-  def receive(data)
-    puts "Received data: #{data.inspect}"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-
-  def can_flip(player = nil)
-    game = Game.find(@game.id)
-    has_turn = game.turn == player.guest_id
-    # cards flipped up by player, and has no scoredBy
-    flipped_cards = flipped_cards_by_player(player)
-
-    matching_number_set = flipped_cards.first&.matching_number_set
-    non_matching_cards = flipped_cards.select do |flipped_game_card|
-      !matching_number_set.include?(flipped_game_card.card.number)
-    end
-
-    has_turn && flipped_cards.length < max_flipable_cards && non_matching_cards.length == 0
+  # CLIENT ACTIONS (actions sent in from client)
+  def get_game
+    broadcast_game
   end
 
   # GAME ACTIONS
   def flip_card(data)
-    if !can_flip(current_player)
+    @game = Game.find(@game.id)
+    if !@game.can_flip?(current_player)
       puts "Player #{current_player.guest_id} cannot flip a card."
       return
     end
 
-    game = Game.find(@game.id)
-    game_card = game.game_cards.find(data["game_card_id"])
-    game_card.face_up = true
-    game_card.flipped_by = current_player.guest_id
-    game_card.touch
-    game_card.save
+    game_card = @game.game_cards.find(data["game_card_id"])
+    game_card.update(
+      face_up: true,
+      flipped_by: current_player.guest_id
+    )
+    puts "======= game_card flipped by player #{current_player.guest_id} ======="
+    puts game_card.inspect
+    broadcast_game
 
-    get_game
-    reset = progress_game(game_card)
-    get_game(reset)
+    # game progresses / validation of action, then broadcast the game state
+    delay_update = Game.find(@game.id).progress_game(current_player)
+    broadcast_game({ delay: delay_update ? 1000 : 0 })
   end
 
   def concede(data)
     puts "Player #{current_player&.guest_id} conceded the game."
-    game_player_idx = @game.game_players.find_index { |gp| gp.player == current_player }
-    opponent_player_idx = @game.game_players.find_index { |gp| gp.player != current_player }
+    current_player.update_attribute!(:score, -1)
+    opponent_player.update_attribute!(:score, 1)
+    @game.update_attribute!(:state, "finished")
 
-    @game.game_players[game_player_idx].score = -1
-    @game.game_players[game_player_idx].save
-    @game.game_players[opponent_player_idx].score = 1
-    @game.game_players[opponent_player_idx].save
-    @game.state = "finished"
-
-    @game.save
-
-    get_game
+    broadcast_game
   end
 
   private
-  def max_flipable_cards
-    max = 2
 
-    if flipped_cards.length > 0 && flipped_cards.first.card.card_sets.first.cards.length > 2
-      max = 3
-    end
-
-    puts "========== max_flipable_cards = #{max} ==========="
-    max
-  end
-
-
-  def progress_game(flipped_card = nil)
+  def broadcast_game(opts = {})
     game = Game.find(@game.id)
-    flipped_cards = flipped_cards_by_player(current_player)
 
-    return false if !flipped_cards || flipped_cards.length < 2
-
-    matching_number_set = flipped_cards.first&.matching_number_set
-
-    non_matching_cards = flipped_cards.select do |flipped_game_card|
-      !matching_number_set.include?(flipped_game_card.card.number)
-    end
-    puts "============= matching_number_set = #{matching_number_set} ============="
-    puts "============= flipped_cards = #{flipped_cards.map(&:card).map(&:number)} ============="
-    puts "============= non_matching_cards = #{non_matching_cards.map(&:card).map(&:number)} ============="
-
-    if non_matching_cards.length > 0
-      puts "Cards Do not Match by player #{current_player.guest_id}."
-      flipped_cards.each do |card|
-        card.flipped_by = nil
-        card.face_up = false
-        card.save
-      end
-      game.turn = opponent_player.guest_id
-    elsif !(flipped_cards.length == 2 && matching_number_set.length == 3)
-      puts "Cards matched by player #{current_player.guest_id}."
-      flipped_cards.each do |card|
-        card.scored_by = current_player.guest_id
-        card.save
-      end
-      game.turn = opponent_player.guest_id
-    end
-
-    # all cards flipped up by player, and has scoredBy
-    all_cards_used = game.game_cards.filter do |gc|
-      gc.face_up? && gc.scored_by
-    end.length == 16
-
-    if all_cards_used
-      player_flipped_cards = game.game_cards.select { |gc| gc.scored_by == current_player.guest_id }.length
-      opponent_flipped_cards = game.game_cards.select { |gc| gc.scored_by == opponent_player.guest_id }.length
-
-      game.players.each do |player|
-        game_player = game.game_players.find { |gp| gp.player.guest_id == player.guest_id }
-        if player.guest_id == current_player.guest_id
-          game_player.score = player_flipped_cards
-        else
-          game_player.score = opponent_flipped_cards
-        end
-        game_player.save
-      end
-      game.state = "finished"
-    end
-
-    current_player.save
-    game.save
-    non_matching_cards.length > 0 # determines if the game should 'reset' the flipped cards
-  end
-
-  def flipped_cards
-    game = Game.find(@game.id)
-    game.game_cards.select { |gc| gc.face_up? && gc.scored_by.nil? }
-  end
-
-  def flipped_cards_by_player(player)
-    game = Game.find(@game.id)
-    game.game_cards.select { |gc| gc.face_up? && gc.flipped_by == player&.guest_id && gc.scored_by.nil? }
-  end
-
-  def generate_card(game_card)
-    gc = { id: game_card.id, flipped: game_card.face_up }
-    if game_card.face_up?
-      gc[:image_url] = game_card.image_url
-    end
-    gc
-  end
-
-  def guest_id
-    connection.guest_id
+    GamesChannel.broadcast_to(game,
+      game.stream(opts)
+    )
   end
 
   def init_game
@@ -238,16 +113,15 @@ class GamesChannel < ApplicationCable::Channel
       puts "Joined existing game with ID: #{game.id}"
     end
 
-    @game = game
+    @game = Game.find(game.id)
   end
 
   def current_player
-    Player.find_by(guest_id: guest_id)
+    Player.find_by(guest_id: connection.guest_id)
   end
 
   def opponent_player
-    game = Game.find(@game.id)
-    game.players.find { |p| p.guest_id != current_player.guest_id }
+    @game.players.find { |p| p.guest_id != current_player.guest_id }
   rescue StandardError => e
     puts "Error finding opponent player: #{e.message}"
     nil
--- app/models/game.rb
+++ app/models/game.rb
@@ -13,14 +13,160 @@ class Game < ApplicationRecord
   validates :cards, length: { is: TOTAL_CARDS }, if: :enough_players?
   before_validation :set_cards, if: :needs_cards?
 
-  def enough_players?
-    self.players.length == 2
+  def stream(opts = {})
+    game_stream = {
+      game: stream_game
+    }
+
+    if opts[:delay]
+      game_stream[:delay] = opts[:delay]
+    end
+
+    game_stream
   end
 
-  def needs_cards?
-    self.cards.length < TOTAL_CARDS && enough_players?
+  def flipped_game_cards(player = nil)
+    if player.nil? || player.guest_id.nil?
+      self.game_cards.select { |gc| gc.face_up? && gc.scored_by.nil? }
+    else
+      self.game_cards.select { |gc| gc.face_up? && gc.flipped_by == player.guest_id && gc.scored_by.nil? }
+    end
   end
 
+  def can_flip?(player = nil)
+    player_flipped_game_cards = flipped_game_cards(player)
+    has_turn = self.turn == player.guest_id
+
+    # dynamically check if player can flip cards based on the game state
+    return false if player.nil? || self.state != "playing" || !has_turn
+
+    return true if player_flipped_game_cards.length < 2
+
+
+    # we can just check for the first flipped card's matching number set
+    # as all cards need to match *a* set
+    matching_number_set = player_flipped_game_cards&.first.matching_number_set
+    # any flipped cards that doesn't match the first flipped card's set
+    non_matching_cards = player_flipped_game_cards.select do |flipped_game_card|
+      !matching_number_set.include?(flipped_game_card.card.number)
+    end
+    # when 2+ cards are flipped, and there's a non-matching card
+    # then the set does not match
+    non_matching_set = non_matching_cards.length > 0 && player_flipped_game_cards.length > 1
+
+    puts "============ can_flip? player: #{player.guest_id.last(4)} ============"
+    puts "has_turn: #{has_turn}"
+    puts "player_flipped_game_cards.length: #{player_flipped_game_cards.length}"
+    puts "max_flippable_cards: #{max_flippable_cards}"
+    puts "non_matching_set: #{non_matching_set}"
+
+    # in order for a player to flip cards:
+    # 1. they must have the turn
+    # 2. they must have less than 3 flipped
+    # 3. there must not be a non-matching card in the flipped cards
+    has_turn && (player_flipped_game_cards.length < max_flippable_cards) && !non_matching_set
+  end
+
+  def progress_game(player = nil)
+    player_flipped_game_cards = flipped_game_cards(player)
+    puts "============ progress_game ============"
+    puts "player: #{player&.guest_id}"
+    puts "player_flipped_game_cards: #{player_flipped_game_cards.inspect}"
+    return false if player.nil? || !player_flipped_game_cards || player_flipped_game_cards.length < 2
+
+    matching_number_set = player_flipped_game_cards.first.matching_number_set
+
+    non_matching_cards = player_flipped_game_cards.select do |flipped_game_card|
+      !matching_number_set.include?(flipped_game_card.card.number)
+    end
+
+    all_matching_cards = non_matching_cards.length == 0 &&
+      player_flipped_game_cards.length >= matching_number_set.length
+
+    opponent_player = self.players.find { |p| p.guest_id != player.guest_id }
+
+    if non_matching_cards.length > 0
+      player_flipped_game_cards.each do |game_card|
+        game_card.update({
+          flipped_by: nil,
+          face_up: false
+        })
+      end
+      self.update({ turn: opponent_player.guest_id })
+    elsif all_matching_cards
+      puts "Cards matched by player #{player.guest_id}."
+      player_flipped_game_cards.each do |game_card|
+        game_card.update({ scored_by: player.guest_id })
+      end
+      self.update({ turn: opponent_player.guest_id })
+    end
+
+    if all_game_cards_flipped?
+      opponent = self.players.find { |p| p.guest_id != player.guest_id }
+      player_scored_game_cards = self.game_cards.select { |gc| gc.scored_by == player.guest_id }.length
+      opponent_scored_game_cards = self.game_cards.select { |gc| gc.scored_by == opponent.guest_id }.length
+
+      self.game_players.each do |game_player|
+        if game_player.player.guest_id == player.guest_id
+          score = player_scored_game_cards
+        else
+          score = opponent_scored_game_cards
+        end
+        game_player.update({ score: score })
+      end
+
+      self.update({ state: "finished" })
+    end
+
+    non_matching_cards.length > 0 # determines if the game should 'reset' the flipped cards
+  end
+
+  private
+  # STREAM DATA
+  # This is the data that will be sent to the client via ActionCable
+  def stream_game
+    {
+      id: self.id,
+      state: self.state,
+      players: stream_players,
+      cards: stream_cards,
+      winner: winner_id
+    }
+  end
+
+  def stream_players
+    self.players.map do |player|
+      {
+        id: player.guest_id,
+        score: self.game_cards.select { |gc| gc.scored_by == player.guest_id }.length,
+        can_flip: can_flip?(player)
+      }
+    end
+  end
+
+  def stream_cards
+    self.game_cards.sort_by(&:position).map do |game_card|
+      {
+        id: game_card.id,
+        flipped: game_card.face_up,
+        image_url: game_card.face_up? ? game_card.image_url : nil
+      }
+    end
+  end
+
+  # STREAM HELPERS
+  def max_flippable_cards
+    # default is 2 unless there's a set of 3
+    max = 2
+
+    if flipped_game_cards.length > 0 && flipped_game_cards.first.matching_number_set.length > 2
+      max = 3
+    end
+
+    max
+  end
+
+  # MODEL ACTIONS
   def set_cards
     total_cards = TOTAL_CARDS
 
@@ -41,13 +187,28 @@ class Game < ApplicationRecord
       if set.cards.length == 1
         self.cards.concat([ set.cards.first, set.cards.first ])
       else
-        self.cards.concat(set.cards)
+        self.cards.concat(set.cards.shuffle)
       end
     end
 
     self.game_cards.shuffle.each_with_index do |game_card, index|
       game_card.position = index
       game_card.save
+    end
+  end
+
+  # DYNAMIC ATTRIBUTES
+  def enough_players?
+    self.players.length == 2
+  end
+
+  def needs_cards?
+    self.cards.length < TOTAL_CARDS && enough_players?
+  end
+
+  def all_game_cards_flipped?
+    self.game_cards.all? do |game_card|
+      game_card.face_up? && game_card.scored_by
     end
   end
 
