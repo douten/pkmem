e560a187f697d78245b8b84afaa5c03c7b73c93b
Minh Nguyen <minh.n@hey.com>
2025-07-03 11:28:09 -0400

    pkm-41 cors for new domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://192.168.86.230:5173"
+    origins "http://192.168.86.230:5173", "https://pkm.minh.codes"
 
     resource "*",
       headers: :any,
