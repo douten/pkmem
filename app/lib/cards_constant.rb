38a230c93a199b40d3d3944adbb1a094bb5d096c
Minh Nguyen <minh.n@hey.com>
2025-07-21 15:55:35 -0400

    fix promo

--- app/lib/cards_constant.rb
+++ app/lib/cards_constant.rb
@@ -23,17 +23,18 @@ module CardsConstant
         # Format: A1a_002_EN.png using modified set case and padded card number
         card_number = card[:number].to_s.rjust(3, "0")
 
-        # Handle promo cards vs regular cards
-        if card[:set].start_with?("P")
-          # Promo cards use P-A format
-          set_case = "P-A"
+        # Some promo card URLs do not exist in the expected format.
+        # So we'll skip them. Otherwise use set_case like below if it starts with "P":
+        #  set_case = "P-A"
+
+        # Skip promo cards entirely
+        next if card[:set].start_with?("P")
+
+        # Convert set like "A1A" to "A1a" (make only the last letter lowercase)
+        if card[:set].length > 2
+          set_case = card[:set][0..-2] + card[:set][-1].downcase
         else
-          # Convert set like "A1A" to "A1a" (make only the last letter lowercase)
-          if card[:set].length > 2
-            set_case = card[:set][0..-2] + card[:set][-1].downcase
-          else
-            set_case = card[:set]
-          end
+          set_case = card[:set]
         end
 
         tcg_by_name[normalized_name] << "https://limitlesstcg.nyc3.cdn.digitaloceanspaces.com/pocket/#{set_case}/#{set_case}_#{card_number}_EN.png"
