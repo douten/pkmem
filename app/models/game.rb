35da6f713c8a6ae82f8fd3578ead0046382a990f
Minh Nguyen <minh.n@hey.com>
2025-07-23 14:39:50 -0400

    PKM-89 GameStateValidation

--- app/models/game.rb
+++ app/models/game.rb
@@ -16,32 +16,7 @@ class Game < ApplicationRecord
     abandoned: "abandoned",
     error: "error"
   }
-  TERMINAL_STATES = %w[finished abandoned error].freeze
-  UNPLAYABLE_STATES = %w[abandoned error].freeze
-
-  # validate states
-  # includes correct states
-  validates :state, inclusion: { in: states.keys }, if: -> { state_changed? }
-  # matching -> playing, error
-  validates :state, inclusion: { in: %w[playing error],
-      message: ->(object, _) { state_machine_error_message(object) }
-    }, if: -> { state_changed? && state_was == "matching" }
-  # # playing -> finished, abandoned, error
-  validates :state, inclusion: { in: TERMINAL_STATES,
-      message: ->(object, _) { state_machine_error_message(object) }
-    }, if: -> { state_changed? && state_was == "playing" }
-  # finished, abandoned, error -> no transitions
-  validates :state, inclusion: { in: [],
-      message: ->(object, _) { state_machine_error_message(object) }
-    }, if: -> { state_changed? && TERMINAL_STATES.include?(state_was) }
-
-  # validates that cards exist when there's two players
-  validates :cards, length: { minimum: ->(game) { game.total_cards_in_game } }, if: :enough_players?
-  before_validation :set_cards, if: :needs_cards?
-
-  # Callbacks
-  # Remove game_cards when game terminates to free up records
-  before_save :remove_game_cards, if: -> { will_save_change_to_state? && UNPLAYABLE_STATES.include?(state) }
+  include GameStateValidations
 
   def stream(opts = {})
     game_stream = {
